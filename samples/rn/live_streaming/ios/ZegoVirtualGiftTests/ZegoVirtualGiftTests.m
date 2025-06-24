75744ff4a23ed1ef1749b9b75b17f4b286676059
tripli <tripli@zego.im>
2025-06-24 18:40:00 +0800

    Update React Native version to 0.76.9 and rename project.

--- samples/rn/live_streaming/ios/ZegoVirtualGiftTests/ZegoVirtualGiftTests.m
+++ samples/rn/live_streaming/ios/ZegoVirtualGiftTests/ZegoVirtualGiftTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ZegoPrebuiltLiveStreamingTests : XCTestCase
+@interface ZegoVirtualGiftTests : XCTestCase
 
 @end
 
-@implementation ZegoPrebuiltLiveStreamingTests
+@implementation ZegoVirtualGiftTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
